797cc063715fe6e57ab71fcc3f01275492fa051f
Cory Kleiser <cory.kleiser@gmail.com>
2019-07-24 19:01:40 -0400

    updated comment

--- src/price-calculator.js
+++ src/price-calculator.js
@@ -4,7 +4,7 @@
 
 /*** Changes
  ES5 => ES6+
- Updated to use closures instead of making function global. (thinking of making the global function that sets everything up a class or object but would have to change the index.html)
+ Updated to use closures instead of making function global
  Created enumerations that return the userType/productType in plain english for readability
  Separated concerns by creating a separate function for product type price and rebate amount
 ***/
