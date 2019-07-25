fe38eaae7b035dc871c3dfada5507c912a08fef8
Cory Kleiser <cory.kleiser@gmail.com>
2019-07-24 20:06:28 -0400

    added enums for ProductPremium and UserRebate

--- src/price-calculator.js
+++ src/price-calculator.js
@@ -9,7 +9,7 @@
  Separated concerns by creating a separate function for product type price and rebate amount
  
  Improvements:
- One large improvement to my refactored code would be to update the enumerations to do something 
+ One improvement to my refactored code would be to update the enumerations to do something
  more valuable. Currently they just serve to make the code more readable for the developer. Instead of that,
  maybe update the value to the rebate amount and then use that instead of checking
  in an if statement. This way when we want to update a rebate amount, for example, we could just 
@@ -24,11 +24,25 @@ const calculatePrice = (() => {
     0: 'normal',
     1: 'company'
   }
+  // define user rebate amounts
+  const UserRebate = {
+    'normal': 0,
+    'company': 5
+  }
+  // define new product rebate amount
+  const newProductRebate = 10
+
 
   // define product types
   const ProductTypes = {
     0: 'new',
     1: 'old'
+  }
+
+  // define Product Type Premiums
+  const ProductPremium = {
+    'new': 25,
+    'old': 35
   }
 
   /**
@@ -40,8 +54,8 @@ const calculatePrice = (() => {
    */
   const calculateRebate = (userType, productType, publishedDate) => {
     let rebate = 0;
-    if (UserTypes[userType] == 'company') rebate += 5 // no need for triple equals because we no UserTypes are strings (no need for type casting)
-    if (ProductTypes[productType] == 'new' && publishedDate.toDateString() == new Date().toDateString()) rebate += 10
+    if (UserTypes[userType] == 'company') rebate += UserRebate[UserTypes[userType]] // no need for triple equals because we no UserTypes are strings (no need for type casting)
+    if (ProductTypes[productType] == 'new' && publishedDate.toDateString() == new Date().toDateString()) rebate += newProductRebate
     return rebate
   }
 
@@ -50,7 +64,7 @@ const calculatePrice = (() => {
    * @param {number} productType
    * @returns {number} productTypePrice
    */
-  const calculateProductTypePrice = (productType) => ProductTypes[productType] == 'new' ? 25 : 35
+  const calculateProductTypePrice = (productType) => ProductPremium[ProductTypes[productType]]
 
   return (userType, productType, price, publishedDate) => {
     try	{
